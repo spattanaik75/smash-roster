0160f07fc77c0e2e58bacaeb64953a0d4fe0eba9
Soumya Pattanaik <Soumya.Pattanaik@securitease.com>
2026-02-23 12:43:31 +0200

    test

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/smash-roster/dev-sw.js?dev-sw', { scope: '/smash-roster/', type: 'classic' })
